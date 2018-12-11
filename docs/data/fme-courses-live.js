4bcd0e459e9446a9ba5f64a732324065852148ea
Joao F. Ferreira <joao@joaoff.com>
2018-12-11 15:43:38 +0000

    Updated webpage to contain filtered courses with active web links

---
+++ docs/data/fme-courses-live.js
@@ -0,0 +1,222 @@
+var courses = [
+{
+  "name": "<a href=\"http://dept-info.labri.u-bordeaux.fr/~griffaul/Enseignement/MFIL/\">Formal Methods in Software Engineering</a>",
+  "year_level": "Unknown",
+  "contact": "<a href=\"http://dept-info.labri.u-bordeaux.fr/~griffaul\">A. Griffault</a>",
+  "offered_at": "<a href=\"http://www.u-bordeaux1.fr/\">Bordeaux</a>",
+  "topics": [ "Unknown" ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://www.cl.cam.ac.uk/DeptInfo/CST/node22.html\">Software Engineering II</a>",
+  "year_level": "Unknown",
+  "contact": "L.C. Paulson",
+  "offered_at": "<a href=\"http://www.cl.cam.ac.uk/\">Cambridge</a>",
+  "topics": [ "Algorithm refinement" ],
+  "languages_tools": [
+    "Z-Notation",
+    "Standard ML"
+  ]
+},
+{
+  "name": "<a href=\"http://www.dcs.ed.ac.uk/home/dts/fps/\">Functional Programming and Specification</a>",
+  "year_level": "Unknown",
+  "contact": "<a href=\"http://www.dcs.ed.ac.uk/~dts\">D. Sannella</a>",
+  "offered_at": "Edinburgh",
+  "topics": [ "Unknown" ],
+  "languages_tools": [
+    "Standard ML",
+    "Extended ML"
+  ]
+},
+{
+  "name": "<a href=\"http://www.doc.ic.ac.uk/~mrh/302/home.html\">Software Engineering - Methods</a>",
+  "year_level": "Unknown",
+  "contact": "<a href=\"http://www.doc.ic.ac.uk/~mrh/\">M. Huth</a>",
+  "offered_at": "<a href=\"http://www.doc.ic.ac.uk/\">Imperial C.</a>",
+  "topics": [ "Formal program techniques" ],
+  "languages_tools": [ "Alloy" ]
+},
+{
+  "name": "<a href=\"http://www.dil.univ-mrs.fr/dea/Pages/coursDea2000.html\">Techniques de preuve et v\u00e9rification formelle</a>",
+  "year_level": "MSc",
+  "contact": "J.-R. Abrial",
+  "offered_at": "Marseille",
+  "topics": [
+    "Abstract State Machines",
+    "Model Checking"
+  ],
+  "languages_tools": [
+    "B-Method"
+  ]
+},
+{
+  "name": "<a href=\"http://www.elet.polimi.it/upload/mandriol/Didattica/sitofms.html\">Formal methods for concurrent and distributed systems</a>",
+  "year_level": "4",
+  "contact": "D. Mandrioli",
+  "offered_at": "Milan Poli.",
+  "topics": [
+    "Hoare Logic",
+    "Refinement in B",
+    "Petri Nets",
+    "Temporal Logic"
+  ],
+  "languages_tools": [
+    "B-Method",
+    "TRIO"
+  ]
+},
+{
+  "name": "<a href=\"http://www.elet.polimi.it/upload/sanpietr/SE/SE-Mi.html\">Software engineering II</a>",
+  "year_level": "4",
+  "contact": "<a href=\"http://www.elet.polimi.it/upload/sanpietr/index.html\">P. San Pietro</a>",
+  "offered_at": "Milan Poli.",
+  "topics": [
+    "Model Checking",
+    "Petri Nets"
+  ],
+  "languages_tools": [ "SPIN" ]
+},
+{
+  "name": "<a href=\"http://www.elet.polimi.it/upload/mezzalir/InfInd_VO\">Industrial automation II</a>",
+  "year_level": "4/5",
+  "contact": "L. Mezzalira",
+  "offered_at": "Milan Poli.",
+  "topics": [ "Petri Nets" ],
+  "languages_tools": [ "TRIO" ]
+},
+{
+  "name": "<a href=\"http://www.di.uminho.pt/~jno/html/mpi.html\">Programming Methodology I</a>",
+  "year_level": "2",
+  "contact": "<a href=\"http://www.di.uminho.pt/~jno\">J.N. Oliveira</a>",
+  "offered_at": "Minho",
+  "topics": [
+    "Functional Programming",
+    "Algebra of Programming"
+  ],
+  "languages_tools": [
+    "Haskell",
+    "Hugs",
+    "GHC"
+  ]
+},
+{
+  "name": "<a href=\"http://www.di.uminho.pt/~jno/html/mii.html\">Formal Methods II</a>",
+  "year_level": "4",
+  "contact": "<a href=\"http://www.di.uminho.pt/~jno\">J.N. Oliveira</a>",
+  "offered_at": "Minho",
+  "topics": [
+    "Algebra of Programming",
+    "Data refinement",
+    "Mathematics of Program Construction"
+  ],
+  "languages_tools": [
+    "VDM-SL",
+    "VDM++",
+    "VDMTools"
+  ]
+},
+{
+  "name": "<a href=\"http://www.di.uminho.pt/~jno/html/labmf.html\">Formal Methods Laboratory</a>",
+  "year_level": "5",
+  "contact": "<a href=\"http://www.di.uminho.pt/~jno\">J.N. Oliveira</a>",
+  "offered_at": "Minho",
+  "topics": [ "Unknown" ],
+  "languages_tools": [
+    "Alloy",
+    "B-Method",
+    "VDM++",
+    "VDMTools"
+  ]
+},
+{
+  "name": "<a href=\"http://www.cs.nott.ac.uk/~rcb/G51MPC/G51MPC.html\">Mathematics of Program Construction</a>",
+  "year_level": "1",
+  "contact": "<a href=\"http://www.cs.nott.ac.uk/~rcb/\">R. Backhouse</a>",
+  "offered_at": "<a href=\"http://www.cs.nott.ac.uk/\">Nottingham</a>",
+  "topics": [ "Mathematics of Program Construction" ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://www.cs.nott.ac.uk/~rcb/G53PAL/G53PAL.html\">Programming Algebra</a>",
+  "year_level": "3",
+  "contact": "<a href=\"http://www.cs.nott.ac.uk/~rcb/\">R. Backhouse</a>",
+  "offered_at": "<a href=\"http://www.cs.nott.ac.uk/\">Nottingham</a>",
+  "topics": [
+    "Algebra of Programming",
+    "Fixed Point Calculus",
+    "Polytypism"
+  ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://web.comlab.ox.ac.uk/oucl/courses/topics00-01/fpdi/index.html\">Formal Program Design I</a>",
+  "year_level": "Unknown",
+  "contact": "<a href=\"http://web.comlab.ox.ac.uk/oucl/people/geraint.jones.html\">G. Jones</a>",
+  "offered_at": "<a href=\"http://www.comlab.ox.ac.uk/oucl\">Oxford</a>",
+  "topics": [ "Algorithm refinement" ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://web.comlab.ox.ac.uk/oucl/courses/topics00-01/fpdii/index.html\">Formal Program Design II</a>",
+  "year_level": "Unknown",
+  "contact": "<a href=\"http://web.comlab.ox.ac.uk/oucl/people/richard.bird.html\">R. Bird</a>",
+  "offered_at": "<a href=\"http://www.comlab.ox.ac.uk/oucl\">Oxford</a>",
+  "topics": [ "Data refinement" ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://web.comlab.ox.ac.uk/oucl/courses/topics00-01/lc/index.html\">Lambda Calculus</a>",
+  "year_level": "Unknown",
+  "contact": "<a href=\"http://web.comlab.ox.ac.uk/oucl/people/andrew.ker.html\">A. Ker</a>",
+  "offered_at": "<a href=\"http://www.comlab.ox.ac.uk/oucl\">Oxford</a>",
+  "topics": [
+    "Functional Programming"
+  ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://web.comlab.ox.ac.uk/oucl/courses/topics00-01/aops/index.html\">Application-Oriented Program Semantics</a>",
+  "year_level": "Unknown",
+  "contact": "J. Sanders",
+  "offered_at": "<a href=\"http://www.comlab.ox.ac.uk/oucl\">Oxford</a>",
+  "topics": [ "Formal Semantics" ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://web.comlab.ox.ac.uk/oucl/courses/topics00-01/dt/synopsis.html\">Domain Theory</a>",
+  "year_level": "Unknown",
+  "contact": "G.M. Reed",
+  "offered_at": "<a href=\"http://www.comlab.ox.ac.uk/oucl\">Oxford</a>",
+  "topics": [
+    "Domains",
+    "Pi-Calculus"
+  ],
+  "languages_tools": [ "Unknown" ]
+},
+{
+  "name": "<a href=\"http://www.lsv.ens-cachan.fr/~phs/notes-VerSysInf.html\">Verification of infinite systems</a>",
+  "year_level": "Unknown",
+  "contact": "<a href=\"http://www.di.unipi.it/~ugo/ugo.html\">U. Montanari</a>",
+  "offered_at": "Pisa",
+  "topics": [
+    "TT "
+  ],
+  "languages_tools": [ "Unknown" ],
+  "#_name": "foundations_of_informatics\nSemantics II \u00a0 "
+},
+{
+  "name": "<a href=\"http://fmt.cs.utwente.nl/courses/fmse/\">Formal Methods for Software Engineering</a>",
+  "year_level": "Unknown",
+  "contact": "E. Brinksma",
+  "offered_at": "Twente",
+  "topics": [ "Process Algebras" ],
+  "languages_tools": [
+    "Z-Notation",
+    "SDL",
+    "FSP",
+    "Z/EVES",
+    "LTSA"
+  ]
+}
+]
